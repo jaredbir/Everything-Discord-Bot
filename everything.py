6da64faad52818337bdb9c06976cb3fb761c2c6b
jaredbir <jaredbir@gmail.com>
2018-09-05 21:53:19 -0700

    Added logging function and began work on voice

--- everything.py
+++ everything.py
@@ -1,41 +1,54 @@
+""" 
+ This is a discord bot that I wrote. It can mention everyone for those pesky servers that turn off the ability to @everyone. 
+ It has the ability for users to search youtube videos in discord and have this bot join the channel the user is in and play the youtube video.
+ I plan on adding some pre-downloaded memes that the bot can play if the user mentions them. This is just a small project that I started and will add to from time to time.
+"""
+
+# Packages that I used
+
 import discord
 import asyncio
+import logging
+import youtube_dl
 
+# Initializes and logs problems and errors that the code has, rather than to the console
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 client=discord.Client()
+
+# Checks if the Opus voice library has loaded, and if it hasn't it will load the Opus library for Voice use
 
 if not discord.opus.is_loaded():
 	discord.opus.load_opus('opus')
 
-class VoiceEntry:
-    def __init__(self, message, player):
-        self.requester = message.author
-        self.channel = message.channel
-        self.player = player
-    def __str__(self):
-        fmt = '*{0.title}* uploaded by {0.uploader} and requested by {1.display_name}'
-        duration = self.player.duration
-        if duration:
-            fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
-        return fmt.format(self.player, self.requester)
+# On ready function that runs as soon as the bot has loaded and is ready to process info
 
-
-@client.event
+@client.event 
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-    
+
+# On message function that runs when a message is sent in the discord server or to the bot directly. There are multiple outcomes, and uses the prefix of ! to call functions.
+
 @client.event
 async def on_message(message):
 	msg=""
 	if message.content.startswith('!everybody'):
 		for member in message.server.members:
 			msg+="<@"+str(member.id)+">\n"
-	
+	if len(msg)!=0:
+		await client.send_message(message.channel, msg)
+	if message.content.startswith('!search') & message.author.voice_channel!=none:
+		chan=message.author.voice_channel
 
-	await client.send_message(message.channel, msg)
-		
+# Runs the discord secret token so that the bot can run, and reads it from a file which is not uploaded.
+
 f=open("discord.key","r")
 key=f.readline()	
 client.run(key)
